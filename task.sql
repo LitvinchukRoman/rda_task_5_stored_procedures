29690cfebb052f1e47e40aa38c3709b8cc4e9a62
litvi <litvin4uk.roman@gmail.com>
2025-07-17 00:41:57 +0300

    Solution

--- task.sql
+++ task.sql
@@ -1,14 +1,15 @@
-USE ShopDB; 
+USE ShopDB;
 
 DELIMITER //
+
 CREATE PROCEDURE get_warehouse_product_inventory(
-    IN ID INT
+    IN product_id INT
 )
 BEGIN
-    SELECT Products.Name, WarehouseAmount
+    SELECT Products.Name, ProductInventory.WarehouseAmount
     FROM ProductInventory
-    JOIN Products
-    ON ProductInventory.ProductID = Products.ID
-    WHERE Products.ID = ID;
+    JOIN Products ON ProductInventory.ProductID = Products.ID
+    WHERE Products.ID = product_id;
 END //
+
 DELIMITER ;
